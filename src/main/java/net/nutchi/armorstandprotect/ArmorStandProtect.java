afdf7a531870a952292f28508637924c24515478
e2318501 <e2318501@outlook.com>
2024-06-21 02:08:52 +0900

    chore: init

---
+++ src/main/java/net/nutchi/armorstandprotect/ArmorStandProtect.java
@@ -0,0 +1,26 @@
+package net.nutchi.armorstandprotect;
+
+import org.bukkit.GameMode;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArmorStandProtect extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    @EventHandler
+    public void onArmorStandManipulate(PlayerArmorStandManipulateEvent event) {
+        if (event.getPlayer().getGameMode().equals(GameMode.ADVENTURE)) {
+            event.setCancelled(true);
+        }
+    }
+}
